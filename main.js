67ba9b1d651893ad89d279fb3cc43cdc3d28ae5b
Astrd1594 <91772903+Astrd1594@users.noreply.github.com>
2022-06-08 22:22:04 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+img = "";
+status = "";
+objects = [];
+function preload(){
+
+}
+function setup(){
+
+}
+function draw(){
+
+}
+function modelLoaded(){
+
+}
+function gotResult(){
+
+}
+function start_search(){
+    
+}
